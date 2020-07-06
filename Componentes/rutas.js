d9de1af41cf6eae37035eb381fb9490c9ace8861
Diego Romero <dr87152@gmail.com>
2020-07-06 11:07:10 -0500

    creacion de los archivos de rutas y el router

---
+++ Componentes/rutas.js
@@ -0,0 +1,15 @@
+const routes =[
+    {
+        path:'/',
+        template: '<h1>HOLA</h1>'
+    },
+    {
+        path:'/contacto',
+        template: '<h1>contacto</h1>'
+    },
+    {
+        path:'/aboutme',
+        template: '<h1>soy io</h1>'
+    }
+
+]
